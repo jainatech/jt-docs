b2dc550d4b12d8c4f6bb9a186c2c2ac855a3909b
Jaina <jaina.tech1998@gmail.com>
2023-10-18 16:34:01 +0530

    Added 2 files

---
+++ .spectral.mjs
@@ -0,0 +1,2 @@
+import ruleset from "https://stoplight.io/api/v1/projects/cHJqOjIyMzE3Mg/spectral.js?branch_slug=master&token=d9d1f893-8523-45ad-a63b-42785e7c38ef";
+export default { extends: ruleset };
